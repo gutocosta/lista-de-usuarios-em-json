9044849da57e92cc562968a002e087126389fdb7
Augusto da Silva Costa <gutoscosta@gmail.com>
2019-03-06 15:26:58 -0300

    Percorrendo o json, totalizar as latitudes negativas

--- python/listar_usuarios.py
+++ python/listar_usuarios.py
@@ -9,6 +9,8 @@ tamanhoListaJson = len(conteudoJson)
 listaWebsitesUsuarios = []
 listaJson = resposta.json()
 usuaria_samantha = 'Samantha'
+listaJsonLoc = resposta.json()
+total_usuarios_hem_sul = 0
 
 if tamanhoListaJson:
     for y in conteudoJson:
@@ -18,7 +20,13 @@ if tamanhoListaJson:
         if usuaria_samantha in x['username']:
             email_usuaria_samantha = (x['email'])
             
+    for z in listaJsonLoc:
+        if float(z['address']['geo']['lat']) < 0:
+            total_usuarios_hem_sul += 1            
+            
 print('1) Os websites de todos os usuários:')
 print(listaWebsitesUsuarios)
 print('2) O email da usuária que possui o campo username igual a "Samantha":')
 print(email_usuaria_samantha)
+print('3) EXTRA: Mostrar o total de usuários do hemisfério sul:')
+print(total_usuarios_hem_sul)
