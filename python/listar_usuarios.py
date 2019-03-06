f909c6466ace60bd1fb356f00061fc2c41d3e555
Augusto da Silva Costa <gutoscosta@gmail.com>
2019-03-06 15:21:37 -0300

    A resposta da requisição em json
    E percorrer o conteudo, criando uma lista dos websites dos usuários

--- python/listar_usuarios.py
+++ python/listar_usuarios.py
@@ -3,3 +3,14 @@ import requests
 weburl = 'https://jsonplaceholder.typicode.com/users'
 
 resposta = requests.get(weburl)
+
+conteudoJson = resposta.json()
+tamanhoListaJson = len(conteudoJson)
+listaWebsitesUsuarios = []
+
+if tamanhoListaJson:
+    for y in conteudoJson:
+        listaWebsitesUsuarios.append((y['website']))
+
+print('1) Os websites de todos os usuários:')
+print(listaWebsitesUsuarios)
