29f3be16f1185a5b7ac32f11032595a37bc9272d
Augusto da Silva Costa <gutoscosta@gmail.com>
2019-03-06 15:24:07 -0300

    Percorrendo o json, encontrar o email da usuária

--- python/listar_usuarios.py
+++ python/listar_usuarios.py
@@ -7,10 +7,18 @@ resposta = requests.get(weburl)
 conteudoJson = resposta.json()
 tamanhoListaJson = len(conteudoJson)
 listaWebsitesUsuarios = []
+listaJson = resposta.json()
+usuaria_samantha = 'Samantha'
 
 if tamanhoListaJson:
     for y in conteudoJson:
         listaWebsitesUsuarios.append((y['website']))
 
+    for x in listaJson:
+        if usuaria_samantha in x['username']:
+            email_usuaria_samantha = (x['email'])
+            
 print('1) Os websites de todos os usuários:')
 print(listaWebsitesUsuarios)
+print('2) O email da usuária que possui o campo username igual a "Samantha":')
+print(email_usuaria_samantha)
